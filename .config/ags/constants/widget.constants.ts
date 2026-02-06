87195682641e7c78843731b24be4bf45f946cf9a
OmarKrypton <omarroma9007@gmail.com>
2026-02-06 20:43:58 +0200

    style(widgets): update MoviesViewer icon

--- .config/ags/constants/widget.constants.ts
+++ .config/ags/constants/widget.constants.ts
@@ -135,7 +135,7 @@ export const leftPanelWidgetSelectors: WidgetSelector[] = [
   },
   {
     name: "MoviesViewer",
-    icon: "",
+    icon: "󰿏",
     widget: () => MoviesViewer(),
     enabled: false,
   },
